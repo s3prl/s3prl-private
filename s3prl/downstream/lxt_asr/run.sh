92251c259ec811ee39630bf4d80d5bfb9a4f48a6
Leo Yang <leo19941227@gmail.com>
2021-10-25 05:11:05 +0800

    only 3 lrs

--- s3prl/downstream/lxt_asr/run.sh
+++ s3prl/downstream/lxt_asr/run.sh
@@ -11,7 +11,7 @@ fi
 upstream=$1
 expdir_root=$2
 
-for lr in "1.0e-2" "1.0e-3" "1.0e-4" "1.0e-5";
+for lr in "1.0e-3" "1.0e-4" "1.0e-5";
 do
     expdir=$expdir_root/$upstream/lr$lr
     python3 run_downstream.py -a -m train -u $upstream -d lxt_asr -o config.optimizer.lr=$lr \
