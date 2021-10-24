392a6c5374cae43958d46269b8f9599221346554
Leo Yang <leo19941227@gmail.com>
2021-10-24 14:00:27 -0400

    reorganize

--- s3prl/downstream/sv_lxt/run.sh
+++ s3prl/downstream/sv_lxt/run.sh
@@ -15,7 +15,7 @@ for lr in "1" "1.0e-1" "1.0e-2" "1.0e-3" "1.0e-4";
 do
     for seed in 1 2 3;
     do
-        python3 run_downstream.py -m train -a -u $upstream -d sv_lxt -p $expdir_root/${upstream}/lr${lr}_seed${seed} \
+        python3 run_downstream.py -m train -a -u $upstream -d sv_lxt -p $expdir_root/${upstream}/lr${lr}/seed${seed} \
             -o config.optimizer.lr=$lr \
             --seed $seed
     done
