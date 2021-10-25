5907dd8ef6cc348cbfbca404fd5efd81f6a16e9a
Leo Yang <leo19941227@gmail.com>
2021-10-25 21:45:20 +0800

    asr run script

--- s3prl/downstream/lxt_asr/run.sh
+++ s3prl/downstream/lxt_asr/run.sh
@@ -3,19 +3,30 @@
 set -x
 set -e
 
-if [ $# != "3" ]; then
-    echo $0 [upstream] [expdir_root] [lr: 1.0e-3 \| 1.0e-4 \| 1.0e-5]
+if [ $# -lt "2" ]; then
+    echo $0 [upstream] [expdir_root]
     exit 1
 fi
 
 upstream=$1
-expdir_root=$2
-lr=$3
+shift
+expdir_root=$1
+shift
 
-expdir=$expdir_root/$upstream/lr$lr
-python3 run_downstream.py -a -m train -u $upstream -d lxt_asr -o config.optimizer.lr=$lr,,config.runner.total_steps=10000 \
-    -p $expdir
+if [ -z "$*" ]; then
+    lrs=("1.0e-2" "1.0e-3" "1.0e-4" "1.0e-5")
+else
+    lrs=($*)
+fi
 
-dev_ckpt=$(ls -t $expdir | grep -P ".*dev.*\.ckpt" | head -n 1)  # take the best checkpoint on dev
-python3 run_downstream.py -m evaluate -e $expdir/$dev_ckpt -t lxt_dev > $expdir/dev.result    
-python3 run_downstream.py -m evaluate -e $expdir/$dev_ckpt -t lxt_test > $expdir/test.result
+for lr in "${lrs[@]}";
+do
+    expdir=$expdir_root/$upstream/lr$lr
+    python3 run_downstream.py -a -m train -u $upstream -d lxt_asr -o config.optimizer.lr=$lr,,config.runner.total_steps=10000 \
+        -p $expdir
+
+    dev_ckpt=$(ls -t $expdir | grep -P ".*dev.*\.ckpt" | head -n 1)  # take the best checkpoint on dev
+    python3 run_downstream.py -m evaluate -e $expdir/$dev_ckpt -t lxt_dev > $expdir/dev.result    
+    python3 run_downstream.py -m evaluate -e $expdir/$dev_ckpt -t lxt_test > $expdir/test.result
+done
+
