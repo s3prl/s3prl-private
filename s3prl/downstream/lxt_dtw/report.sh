956c93c31a5a693e01cfd7f1d6ef0c15986c6557
Leo Yang <leo19941227@gmail.com>
2021-10-27 22:36:04 -0400

    support single layer reporting for lxt_dtw

--- s3prl/downstream/lxt_dtw/report.sh
+++ s3prl/downstream/lxt_dtw/report.sh
@@ -12,7 +12,7 @@ if [ ! -d $upstream_dir ]; then
 fi
 
 # MAP
-best_dev_line=$(grep "MAP" $upstream_dir/*/scoring/dev.result | sort -nk 3 | tail -n 1)
+best_dev_line=$(grep "MAP" $upstream_dir/*/scoring/dev.result /dev/null | sort -nk 3 | tail -n 1)
 best_dev_layer=$([[ $best_dev_line =~ layer([0-9]+) ]] && echo ${BASH_REMATCH[1]})
 best_dev_score=$([[ $best_dev_line =~ ([0-9]+\.[0-9]+) ]] && echo ${BASH_REMATCH[1]})
 test_line=$(grep "MAP" $upstream_dir/layer$best_dev_layer/scoring/test.result)
@@ -20,7 +20,7 @@ test_score=$([[ $test_line =~ ([0-9]+\.[0-9]+) ]] && echo ${BASH_REMATCH[1]})
 echo MAP: $(basename $upstream_dir) layer $best_dev_layer: dev $best_dev_score, test $test_score
 
 # EER
-best_dev_line=$(grep "EER" $upstream_dir/*/scoring/dev.result | sort -nrk 3 | tail -n 1)
+best_dev_line=$(grep "EER" $upstream_dir/*/scoring/dev.result /dev/null | sort -nrk 3 | tail -n 1)
 best_dev_layer=$([[ $best_dev_line =~ layer([0-9]+) ]] && echo ${BASH_REMATCH[1]})
 best_dev_score=$([[ $best_dev_line =~ ([0-9]+\.[0-9]+) ]] && echo ${BASH_REMATCH[1]})
 test_line=$(grep "EER" $upstream_dir/layer$best_dev_layer/scoring/test.result)
