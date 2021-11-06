09ce69035369ddde8ac4a9288ac4e4f3a0cbd19c
Leo Yang <leo19941227@gmail.com>
2021-11-06 17:41:35 +0800

    n_seg seed

--- s3prl/downstream/lxt_sid2/explore.sh
+++ s3prl/downstream/lxt_sid2/explore.sh
@@ -21,7 +21,7 @@ projector_dim=0
 n_seg=5
 
 if [ -z "$*" ]; then
-    lrs=("1" "1.0e-1" "1.0e-2" "1.0e-3")
+    lrs=("1.0e-1" "1.0e-2" "1.0e-3" "1")
 else
     lrs=($*)
 fi
@@ -29,7 +29,7 @@ fi
 for lr in "${lrs[@]}";
 do
     expdir=$expdir_root/${n_seg}seg/${min}_${max}secs/projector${projector_dim}/${granularity}/${upstream}/lr${lr}/${seed}seed/
-    python3 run_downstream.py -a -m train -u $upstream -s SID -d lxt_sid2 -o config.optimizer.lr=$lr,,config.downstream_expert.modelrc.projector_dim=$projector_dim,,config.downstream_expert.modelrc.select=$granularity,,config.runner.total_steps=$total_steps,,config.downstream_expert.datarc.min_secs=$min,,config.downstream_expert.datarc.max_secs=$max \
+    python3 run_downstream.py -a -m train -u $upstream -s SID -d lxt_sid2 -o config.optimizer.lr=$lr,,config.downstream_expert.modelrc.projector_dim=$projector_dim,,config.downstream_expert.modelrc.select=$granularity,,config.runner.total_steps=$total_steps,,config.downstream_expert.datarc.min_secs=$min,,config.downstream_expert.datarc.max_secs=$max,,config.downstream_expert.datarc.n_seg=$n_seg,,config.downstream_expert.seed=$seed \
         -p $expdir
 
     last_ckpt=$(ls -t $expdir | grep -P ".*states.*\.ckpt" | head -n 1) # take the last checkpoint for train acc
