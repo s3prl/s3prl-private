1fb68f8d1a4646faf879b555d4f7b1f596fd347b
Leo Yang <leo19941227@gmail.com>
2021-11-07 00:55:36 -0400

    adjust sid run/config

--- s3prl/downstream/lxt_sid/run.sh
+++ s3prl/downstream/lxt_sid/run.sh
@@ -21,11 +21,14 @@ fi
 
 for lr in "${lrs[@]}";
 do
-    expdir=$expdir_root/$upstream/lr$lr
-    python3 run_downstream.py -a -m train -u $upstream -s SID -d lxt_sid -o config.optimizer.lr=$lr \
-        -p $expdir
+    for argmax in 0 1 2;
+    do
+        expdir=$expdir_root/$upstream/argmax${argmax}/lr$lr
+        python3 run_downstream.py -a -m train -u $upstream -s SID -d lxt_sid -o config.optimizer.lr=$lr,,config.downstream_expert.datarc.split_dir=data/superb_all/sid_train_1_argmax${argmax} \
+            -p $expdir
 
-    dev_ckpt=$(ls -t $expdir | grep -P ".*dev.*\.ckpt" | head -n 1)  # take the best checkpoint on dev
-    python3 run_downstream.py -m evaluate -e $expdir/$dev_ckpt -t dev > $expdir/dev.result    
-    python3 run_downstream.py -m evaluate -e $expdir/$dev_ckpt -t test > $expdir/test.result
+        dev_ckpt=$(ls -t $expdir | grep -P ".*dev.*\.ckpt" | head -n 1)  # take the best checkpoint on dev
+        python3 run_downstream.py -m evaluate -e $expdir/$dev_ckpt -t dev > $expdir/dev.result    
+        python3 run_downstream.py -m evaluate -e $expdir/$dev_ckpt -t test > $expdir/test.result
+    done
 done
