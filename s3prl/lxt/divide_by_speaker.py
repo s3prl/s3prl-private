0df6df860a9eb7ab056abc2ed050faebb971805a
Leo Yang <leo19941227@gmail.com>
2021-11-10 08:13:13 +0800

    divide by speaker

--- s3prl/lxt/divide_by_speaker.py
+++ s3prl/lxt/divide_by_speaker.py
@@ -1,22 +1,21 @@
-import os
 import argparse
-import pandas as pd
 from pathlib import Path
 from shutil import copyfile
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--csv", required=True)
+parser.add_argument("--utt2spk", required=True)
 parser.add_argument("--lxt", required=True)
 parser.add_argument("--tgt", required=True)
 args = parser.parse_args()
 tgt = Path(args.tgt)
 tgt.mkdir(exist_ok=True)
 
-csv = pd.read_csv(args.csv)
-for row_id, row in csv.iterrows():
-    spk = row["speaker (gender/id)"].replace(" ", "_")
-    subdir = tgt / spk
-    subdir.mkdir(exist_ok=True)
-    uid = row["utterance_id"]
-    utt_path = Path(args.lxt) / f"{uid}.wav"
-    copyfile(utt_path, subdir / f"{uid}.wav")
+with open(args.utt2spk) as file:
+    for line in file.readlines():
+        utt, spk = line.split(maxsplit=1)
+        utt = utt.strip()
+        spk = spk.strip()
+        subdir = tgt / spk
+        subdir.mkdir(exist_ok=True)
+        utt_path = Path(args.lxt) / f"{utt}.wav"
+        copyfile(utt_path, subdir / f"{utt}.wav")
