8893048d3220673f4798b99ed335e92b37199516
Leo Yang <leo19941227@gmail.com>
2021-11-15 20:30:17 +0800

    report without comma

--- s3prl/downstream/lxt_dtw/report.sh
+++ s3prl/downstream/lxt_dtw/report.sh
@@ -17,7 +17,7 @@ best_dev_layer=$([[ $best_dev_line =~ layer([0-9]+) ]] && echo ${BASH_REMATCH[1]
 best_dev_score=$([[ $best_dev_line =~ ([0-9]+\.[0-9]+) ]] && echo ${BASH_REMATCH[1]})
 test_line=$(grep "MAP" $upstream_dir/layer$best_dev_layer/scoring/test.result)
 test_score=$([[ $test_line =~ ([0-9]+\.[0-9]+) ]] && echo ${BASH_REMATCH[1]})
-echo MAP: $(basename $upstream_dir) layer $best_dev_layer: dev $best_dev_score, test $test_score
+echo MAP: $(basename $upstream_dir) layer $best_dev_layer: dev $best_dev_score test $test_score
 
 # EER
 best_dev_line=$(grep "EER" $upstream_dir/*/scoring/dev.result /dev/null | sort -nrk 3 | tail -n 1)
@@ -25,4 +25,4 @@ best_dev_layer=$([[ $best_dev_line =~ layer([0-9]+) ]] && echo ${BASH_REMATCH[1]
 best_dev_score=$([[ $best_dev_line =~ ([0-9]+\.[0-9]+) ]] && echo ${BASH_REMATCH[1]})
 test_line=$(grep "EER" $upstream_dir/layer$best_dev_layer/scoring/test.result)
 test_score=$([[ $test_line =~ ([0-9]+\.[0-9]+) ]] && echo ${BASH_REMATCH[1]})
-echo EER: $(basename $upstream_dir) layer $best_dev_layer: dev $best_dev_score, test $test_score
+echo EER: $(basename $upstream_dir) layer $best_dev_layer: dev $best_dev_score test $test_score
