289b6c7247d0bdb2caa56bbeb53f198e283b4fd2
Leo Yang <leo19941227@gmail.com>
2022-05-14 18:42:21 +0800

    configurable test for multigpu dataloader

--- test/_test_multigpu_dataloader.py
+++ test/_test_multigpu_dataloader.py
@@ -1,8 +1,11 @@
+import queue
 import pyarrow as pa
+from time import time
 from tqdm import tqdm
 
 import torch
 import torch.nn as nn
+import torch.multiprocessing as mp
 
 from s3prl import hub
 from s3prl.base.output import Output
@@ -24,15 +27,19 @@ class Project(nn.Module):
 
 
 class simple_iterator:
-    def __init__(self) -> None:
+    def __init__(self, secs: int = 10, batch_size: int = 8) -> None:
         self.num = 1000
+        self.secs = secs
+        self.batch_size = batch_size
 
     def __len__(self):
         return self.num
 
     def __iter__(self):
         for i in range(self.num):
-            yield Output(wav=[torch.randn(16000 * 10) for i in range(8)])
+            yield Output(
+                wav=[torch.randn(16000 * self.secs) for i in range(self.batch_size)]
+            )
 
 
 def test_multigpu_dataloader():
@@ -77,9 +84,53 @@ def test_speed():
             )
 
 
+def produce_tensor(q, e):
+    model = Project().cuda(0)
+    model.eval()
+    with torch.no_grad():
+        for batch in simple_iterator(13, 12):
+            batch = batch.to(f"cuda:0")
+            result = model(**batch)
+            result = result.to("cuda:1")
+            q.put(result)
+
+    q.put(None)
+    e.wait()
+
+
+def test_queue():
+    ctx = mp.get_context("forkserver")
+    q = ctx.Queue(maxsize=1)
+    e = ctx.Event()
+
+    p = ctx.Process(target=produce_tensor, args=(q, e))
+    p.start()
+
+    while True:
+        start = time()
+        try:
+            result = q.get_nowait()
+        except queue.Empty:
+            continue
+        else:
+            if result is None:
+                break
+            else:
+                print("get from queue", time() - start)
+                del result
+
+    e.set()
+    p.join()
+
+
 if __name__ == "__main__":
     import logging
+    import argparse
 
     logging.basicConfig(level=logging.INFO)
-    test_multigpu_dataloader()
-    # test_singlegpu_dataloader()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("function")
+    args = parser.parse_args()
+
+    eval(args.function)()
